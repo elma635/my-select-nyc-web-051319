2f498700b2a2382eb218fa6d84d083a34e235ca1
Elma Rahman <elmarahman635@gmail.com>
2019-05-10 23:47:45 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
  name_collection = [] #set an empty array that everything will be collected in.
   while i < collection.length #same process
     if yield(collection[i]) #this is basically set to true bc while #point isn't to specify even or odd, but anything to pass through.
-  name_collection.push(collection[i])
+  name_collection.push(collection[i]) #if it's true, it'll increment & then be passed into [ ] new collection 
 end
       i += 1
     end

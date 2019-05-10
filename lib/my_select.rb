73622fb308f732b802f830d5c54d6416a1705881
Elma Rahman <elmarahman635@gmail.com>
2019-05-10 23:47:05 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,8 @@
 def my_select(collection)
-  i = 0
- name_collection = []
-  while i < collection.length
-    if yield(collection[i])
+  i = 0 #set counter
+ name_collection = [] #set an empty array that everything will be collected in.
+  while i < collection.length #same process
+    if yield(collection[i]) #this is basically set to true #point isn't to specify even or odd, but anything to pass through.
   name_collection.push(collection[i])
 end
       i += 1
@@ -10,4 +10,4 @@ end
     name_collection
 end
 
-#if yield is t, add to array
+#new collection should be {2,4}
